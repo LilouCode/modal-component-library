878c6847077d0575d2ce0715387fb56f26d0097a
LilouCode <lisa.music@outlook.com>
2022-07-25 10:17:02 +0200

    build: config stories settings

--- src/stories/Modal.stories.jsx
+++ src/stories/Modal.stories.jsx
@@ -3,6 +3,16 @@ import { Modal } from "../Modal";
 export default {
     title: 'Modal - Plugin',
     component: Modal,
+    argTypes: {
+        textColor : {control:'color'},
+        btnColor: {control: 'color'},
+        btnBackgroundColor: {control:'color'},
+        wrapperBackgroundColor: {control:'color'},
+        borderRadius: {control: 'radio', options: ['oval', 'round', 'square'] },
+        textFontSize: {sontrol: 'select', options: ["tiny","small","normal","large","super","extra"]},
+        btnFontSize: {sontrol: 'select', options: ["tiny","small","normal","large","super","extra"]}
+
+    }
 
 };
 
@@ -10,5 +20,12 @@ const Template = args => <Modal {...args}/>;
 
 export const Default = Template.bind({});
 Default.args = {
-    text: 'Hello there!'
+    textFontFamily: "Georgia, 'Times New Roman', Times, serif",
+    btnFontFamily: "Georgia, 'Times New Roman', Times, serif",
+    textColor: "#000000",
+    btnColor: "#acdef3",
+    btnBackgroundColor:"#0b0b13",
+    wrapperBackgroundColor: "#ffffff",
+    textContent: 'A modal in a box in a wrapper in a component',
+    textBtn: "Close the modal",
 }
